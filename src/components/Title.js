231cc0a97670a7f116e9d625f32e7a6cb9660cf7
SedatTurkoglu16 <sedatturkoglu16@gmail.com>
2022-02-26 15:44:01 +0300

    UI design finished and form added

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Title.css'
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FireGram</h1>
+      <h2>Your Pictures</h2>
+      <p>You can upload your images.</p>
+    </div>
+  )
+}
+
+export default Title
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import * as firebase from "firebase/app";
+import 'firebase/firestore'
+import 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB1ZessoVi1EScy892gcdVojudaQ03o1Mw",
+    authDomain: "evernote-clone-d91b8.firebaseapp.com",
+    projectId: "evernote-clone-d91b8",
+    storageBucket: "evernote-clone-d91b8.appspot.com",
+    messagingSenderId: "624115260628",
+    appId: "1:624115260628:web:1a714edd7a8bce8c2daf41"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  export const projectStorage = firebase.storage();
+  export const projectFireStorage = firebase.firestore();
